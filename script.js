2fabdcfcab99b7ee1cde6efc2133086d65a7b025
TylerHartwell <thartwell37@gmail.com>
2023-10-22 22:17:10 -0700

    focus next square after number entry

--- script.js
+++ script.js
@@ -19,26 +19,33 @@ document.body.addEventListener("click", e => {
   }
 })
 
+let nextPlace
+
 document.body.addEventListener("keydown", e => {
   e.preventDefault()
 
   if (e.target.isContentEditable && /[1-9]/.test(e.key) && !e.repeat) {
     e.target.innerText = e.key
-    const nextPlace = Number(e.target.parentElement.dataset.place) + 1
+    nextPlace = Number(e.target.parentElement.dataset.place) + 1
     if (nextPlace == 82) {
       e.target.blur()
       return
     }
-    function focusNextSquareNumber() {
-      e.target.removeEventListener("blur", focusNextSquareNumber)
-      const selector = `.square[data-place="${nextPlace.toString()}"] .square-number`
-      grid.querySelector(selector).focus()
-    }
+    const selector = `.square[data-place="${nextPlace.toString()}"] .square-number`
 
-    e.target.addEventListener("blur", focusNextSquareNumber)
-    e.target.blur()
+    e.target.addEventListener("keyup", e => {
+      const currentEl = e.target
+      focusNextSquareNumber(currentEl, selector)
+    })
   }
 })
+
+function focusNextSquareNumber(currentEl, selector) {
+  const nextEl = grid.querySelector(selector)
+  nextEl.contentEditable = true
+  nextEl.focus()
+  currentEl.removeEventListener("keyup", focusNextSquareNumber)
+}
 
 function enterNumber(e) {
   if (!e.target.innerText) {
