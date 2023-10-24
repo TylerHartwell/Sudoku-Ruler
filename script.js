48151723404b27d80cb462110947db94601ecd9b
TylerHartwell <thartwell37@gmail.com>
2023-10-24 11:28:21 -0700

    set grid and check valid placement

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.body.addEventListener("click", e => {
     toggleCandidates()
   }
   if (e.target.classList.contains("square-number")) {
-    if (!isSet) {
+    if (!isSet || e.target.contentEditable) {
       enterNumber(e)
     }
   }
@@ -19,19 +19,32 @@ document.body.addEventListener("click", e => {
       clearGrid()
     }
   }
+  if (e.target.classList.contains("set-grid-btn")) {
+    setGrid()
+  }
 })
 
 document.body.addEventListener("keydown", e => {
   e.preventDefault()
 
+  clearWrong()
   if (e.target.classList.contains("square-number")) {
     const currentPlace = Number(e.target.parentElement.dataset.place)
     if (/[1-9]/.test(e.key) && !e.repeat) {
+      if (!checkValidPlacement(e.key, e.target.parentElement)) {
+        e.target.classList.add("wrong")
+        e.target.innerText = e.key
+        return
+      }
+
       e.target.innerText = e.key
       removeSeenCandidates(e.target.parentElement)
-      movePlaceBy(1)
-    }
-    if (e.key === "ArrowLeft") {
+      if (!isSet) movePlaceBy(1)
+    } else if (e.key === "0" || e.key === "Backspace" || e.key === "Delete") {
+      e.target.innerText = ""
+      removeSeenCandidates(e.target.parentElement)
+      if (!isSet) movePlaceBy(1)
+    } else if (e.key === "ArrowLeft") {
       movePlaceBy(-1)
     } else if (e.key === "ArrowUp") {
       movePlaceBy(-9)
@@ -42,6 +55,7 @@ document.body.addEventListener("keydown", e => {
     }
 
     function movePlaceBy(numPlaces) {
+      clearWrong()
       const nextPlace = (currentPlace + numPlaces + 81) % 81 || 81
       const selector = `.square[data-place="${nextPlace.toString()}"] .square-number`
       const nextEl = grid.querySelector(selector)
@@ -50,7 +64,68 @@ document.body.addEventListener("keydown", e => {
   }
 })
 
+function clearWrong() {
+  const el = document.querySelector(".wrong")
+  if (el) {
+    el.classList.remove("wrong")
+    el.textContent = ""
+  }
+}
+
+function checkValidPlacement(keyString, squareEl) {
+  const rowSquares = Array.from(
+    document.querySelectorAll(
+      `.square[data-row-n='${squareEl.dataset.rowN}'] .square-number`
+    )
+  )
+  const colSquares = Array.from(
+    document.querySelectorAll(
+      `.square[data-col-n='${squareEl.dataset.colN}'] .square-number`
+    )
+  )
+  const boxSquares = Array.from(
+    document.querySelectorAll(
+      `.square[data-box-n='${squareEl.dataset.boxN}'] .square-number`
+    )
+  )
+  if (
+    isAlreadyIn(keyString, rowSquares, squareEl) ||
+    isAlreadyIn(keyString, colSquares, squareEl) ||
+    isAlreadyIn(keyString, boxSquares, squareEl)
+  ) {
+    return false
+  }
+
+  return true
+}
+
+function isAlreadyIn(keyString, squaresGroup, squareEl) {
+  let isIn = false
+  for (const square of squaresGroup) {
+    if (square.textContent == keyString && square.parentElement != squareEl) {
+      isIn = true
+      return isIn
+    }
+  }
+
+  return isIn
+}
+
+function setGrid() {
+  if (isSet) return
+  Array.from(document.querySelectorAll(".square-number")).forEach(
+    squareNumber => {
+      if (squareNumber.textContent != "") {
+        squareNumber.contentEditable = false
+        squareNumber.classList.add("set")
+      }
+    }
+  )
+  isSet = true
+}
+
 function enterNumber(e) {
+  clearWrong()
   e.target.focus()
 }
 
