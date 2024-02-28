8d9c87d8cce5ccb69d9a7fb378f2bc34802113df
TylerHartwell <thartwell37@gmail.com>
2024-02-28 09:49:32 -0800

    full crud with express router

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const express = require("express")
 const path = require("path")
 const logger = require("./middleware/logger")
-const members = require("./Members")
 
 const app = express()
 
 //Init middleware
 // app.use(logger)
 
-// Gets All Members
-app.get("/api/members", (req, res) => res.json(members))
-
-// Get Singe Member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id))
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)))
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
-  }
-})
+// Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // send html directly
 // app.get("/", (req, res) => {
@@ -34,6 +23,9 @@ app.get("/api/members/:id", (req, res) => {
 
 //Set public folder to static so static assets (such as HTML, CSS, images, and client-side JavaScript files) are served directly to clients without any processing by the server. any request that matches a file in the "public" folder will be served directly by Express
 app.use(express.static(path.join(__dirname, "public")))
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"))
 
 const PORT = process.eventNames.PORT || 5000
 
