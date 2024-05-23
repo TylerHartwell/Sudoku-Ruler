670fba364dbdee24c36a481a3e9ce6eef2b82c3b
TylerHartwell <thartwell37@gmail.com>
2024-05-22 19:21:06 -0700

    another one

--- netlify/functions/fetch-grid-string.js
+++ netlify/functions/fetch-grid-string.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch")
+import fetch from "node-fetch"
 
-exports.handler = async function (event, context) {
+export async function handler(event, context) {
   const sudoku_api_url = "https://youdosudoku.com/api/"
 
   try {
